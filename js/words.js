0a223f7b45b69fa3ae98407cc739963a1a37088c
olivier villevieille <oliviervillevieille@hotmail.fr>
2022-10-27 06:43:50 +0200

    first commit

---
+++ js/words.js
@@ -0,0 +1,42 @@
+let words = [
+  {
+    word: "homemade",
+    hint: "Processus qui consiste à faire tout sois-même",
+  },
+  {
+    word: "réunion",
+    hint: "Evènement qui rassemble plusieurs personnes",
+  },
+  {
+    word: "freelance",
+    hint: "Personne qui travaille de manière indépendante",
+  },
+  {
+    word: "javascript",
+    hint: "Language de programmation",
+  },
+  {
+    word: "portfolio",
+    hint: "Support visuel pour présenter son travail",
+  },
+  {
+    word: "linkedin",
+    hint: "Réseau social professionnel",
+  },
+  {
+    word: "voyager",
+    hint: "Verbe qui exprime le fait de se déplacer",
+  },
+  {
+    word: "Italie",
+    hint: "Pays magnifique",
+  },
+  {
+    word: "Firenze",
+    hint: "Un musée à ciel ouvert",
+  },
+  {
+    word: "oliive33",
+    hint: "Mon compte instagram",
+  },
+];
